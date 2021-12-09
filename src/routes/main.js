7eb81e6081282c232de5812161d8f88ee8ae4d18
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2021-12-09 17:04:17 -0600

    allow phone field missing

--- src/routes/main.js
+++ src/routes/main.js
@@ -14,35 +14,21 @@ router.post('/', async (req, res) => {
         const idForm =  req.body.idForm
         const email = req.body.email
         const phone = req.body.phone
-        /*const company = req.body.company
-        const firstname = req.body.firstname
-        const lastname = req.body.lastname
-        const numemployees = req.body.numemployees
-        const role = req.body.role
-        const medio_de_contacto_de_preferencia = req.body.medio_de_contacto_de_preferencia
-        const hs_context = req.body.hs_context 
-        const {email, phone, company, firstname, lastname,numemployees,role,medio_de_contacto_de_preferencia} = req.body */
-        
         const form = new FormData();
+        
         for ( var key in item ) {
             form.append(key, item[key]);
         }
 
-     /*   form.append("company", company)
-        form.append("email", email)
-        form.append("firstname", firstname)
-        form.append("lastname", lastname)
-        form.append("phone", phone)
-        form.append("numemployees", numemployees)
-        form.append("role", role)
-        form.append("medio_de_contacto_de_preferencia", medio_de_contacto_de_preferencia)
-        form.append('hs_context', hs_context) */
-        //const simplePublicObjectInput = {properties}
-        
         const verifyPhone = async phone => {
-            console.log(phone)
-            phonePass = await axios.post(`http://apilayer.net/api/validate?access_key=${process.env.PHONE_KEY}&number=${phone}&country_code=MX&format=1`)
-            return phonePass.data.valid
+            if (phone === "" || !phone ) {
+                phonePass = "Phone is not required"
+            return phonePass
+            } else {
+                console.log(phone)
+                phonePass = await axios.post(`http://apilayer.net/api/validate?access_key=${process.env.PHONE_KEY}&number=${phone}&country_code=MX&format=1`)
+                return phonePass.data.valid
+            }
         }
         const verifyEmail = async email => {
             console.log(email)
@@ -59,7 +45,7 @@ router.post('/', async (req, res) => {
         verify(phone, email).then(async (data) => {
             console.log(data.score)
             console.log(data.valid)
-            if (data.valid !== true) {
+            if (data.valid === false) {
                 res.status(208);
                 res.json({
                     success: false,
@@ -88,52 +74,8 @@ router.post('/', async (req, res) => {
                 }).catch(function (error) {
                     console.error(error);
                 });
-
-                // hubspot.sendToHubSpot(simplePublicObjectInput).then(() => {
-                //     console.log('herecreate1')
-                //     res.json({
-                //         success: true,
-                //         message: 'contacto creado'
-                //     });
-                // }).catch(err => {
-                //     let userId = err.response.body.message
-                //     if (err.statusCode === 409) {
-                //         let regex = /(\d+)/g;
-                //         const contactId = Math.floor(userId.match(regex))
-                //         hubspot.updateToHubspot(contactId, simplePublicObjectInput).then(() => {
-                //             console.log('hereupdate1')
-                //             return res.json({
-                //                 success: true,
-                //                 message: 'contacto actualizado'
-                //             });
-                //         }).catch(e => console.error(e))
-                //     }
-                // })
             }
-            // else if (data.score >= 50) {
-            //     hubspot.sendToHubSpot(simplePublicObjectInput).then(() => {
-            //         console.log('herecreate2')
-            //         res.json({
-            //             success: true,
-            //             message: 'contacto creado'
-            //         });
-            //     }).catch(err => {
-            //         let userId = err.response.body.message
-            //         if (err.statusCode === 409) {
-            //             let regex = /(\d+)/g;
-            //             const contactId = Math.floor(userId.match(regex))
-            //             hubspot.updateToHubspot(contactId, simplePublicObjectInput).then(() => {
-            //                 console.log('hereupdate2')
-            //                 res.json({
-            //                     success: true,
-            //                     message: 'contacto actualizado'
-            //                 });
-            //             }).catch(e => console.error(e))
-            //         }
-            //     })
-            //     console.log('50-74 Bueno')
-            // }
-            else if (data.score >= 25) {
+             else if (data.score >= 25) {
                 console.log('25 -49 regular ')
                 res.status(206);
                 res.json({
