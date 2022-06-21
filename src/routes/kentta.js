c53f5bde598ba71bc78a24bf10b245b5fa15ee16
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2022-06-21 14:53:03 -0500

    kentta route fixed

--- src/routes/kentta.js
+++ src/routes/kentta.js
@@ -16,26 +16,30 @@ router.post('/', async (req, res) => {
        
         
         const verifyPhone = async phone => {
-            if (phone === "" || !phone ) {
-                phonePass = "Falta numero de telefono"
-            return phonePass
-            } else {
+          
                 console.log(phone)
-                phonePass = await axios.post(`http://apilayer.net/api/validate?access_key=${process.env.PHONE_KEY_KENTTA}&number=${phone}&country_code=MX&format=1`)
+                phonePass = await axios.post(`http://apilayer.net/api/validate?access_key=19967b2c366c291d3d1d4c2beced783b&number=${phone}&country_code=MX&format=1`)
                 return phonePass.data.valid
-            }
+            
         }
         const verifyEmail = async email => {
             console.log(email)
-            emailPass = await axios.post(`https://api.emailable.com/v1/verify?email=${email}&api_key=${process.env.EMAIL_KEY_KENTTA}`)
+            emailPass = await axios.post(`https://api.emailable.com/v1/verify?email=${email}&api_key=test_0812bfe7c9532d80ffe1`)
             return emailPass.data.score
         }
         const verify = async (phone, email) => {
-            let valid = await verifyPhone(phone).catch(e => console.error(e))
+            if(phone) {
+                let valid = await verifyPhone(phone).catch(e => console.error(e))
+                let score = await verifyEmail(email).catch(e => console.error(e))
+                console.log(score)
+                console.log(valid)
+                return {valid, score}
+            }
+
             let score = await verifyEmail(email).catch(e => console.error(e))
-            console.log(score)
-            console.log(valid)
-            return {valid, score}
+                console.log(score)
+               
+                return {score}
         }
         verify(phone, email).then(async (data) => {
             console.log(data.score)
@@ -48,16 +52,11 @@ router.post('/', async (req, res) => {
                 });
                 return
             }
-            else if (data.valid === true){
-                res.json({
-                    success: true,
-                    message: 'Numero de telefono  valido'
-                });
-            }
+            
             if (data.score >= 50) {
                 console.log(`${data.score} Muy Bueno`)
                 res.json({
-                    success: false,
+                    success: true,
                     message: 'Correo Valido'
                 });
             }
